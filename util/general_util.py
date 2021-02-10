3be18c89469587fe4766649fd6e50519108face5
Artem.Fediai <artem.fedyay@gmail.com>
2021-02-10 17:43:12 +0100

    unitlities utils module and first functions added: timeit and unit_conversion

---
+++ util/general_util.py
@@ -0,0 +1,22 @@
+def timeit(fn):
+    from time import perf_counter
+    from functools import wraps
+
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        start = perf_counter()
+        result = fn(*args, **kwargs)
+        end = perf_counter()
+        elapsed = end - start
+
+        args_ = [str(a) for a in args]
+        kwargs_ = ['{0}={1}'.format(k, v) for (k, v) in kwargs.items()]
+        all_args = args_ + kwargs_
+        args_str = ','.join(all_args)
+        # print('{0}({1}) took {2:.6f}s to run.'.format(fn.__name__,
+        #                                               args_str,
+        #                                               elapsed))
+        print('{0} took {1:.6f}s to run.'.format(fn.__name__, elapsed))
+        return result
+
+    return inner
---
+++ util/units_conversion.py
@@ -0,0 +1,5 @@
+from scipy.constants import physical_constants as c
+
+
+def eV_to_Hartree():
+    return c['hartree-electron volt relationship'][0]
