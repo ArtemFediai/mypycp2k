79fe5d371d215d2d4682fd4ca5f772fac4a57185
ArtemFediai <artem.fedyay@gmail.com>
2021-03-09 10:40:56 +0100

    run cp2k with mpi from python

--- stand_alone_scripts/argparse/cp2k_mpi.py
+++ stand_alone_scripts/argparse/cp2k_mpi.py
@@ -1,24 +1,44 @@
 #!/home/artem/anaconda3/bin/python
-##!/bin/bash/ python3
+
 import argparse
+import os
 from mpi4py import MPI
+from cp2k_run.cp2k_run import cp2k_run
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-parser = argparse.ArgumentParser(description='Return the argument')
-parser.add_argument('my_argument',
-                    help='my_argument')
+
+parser = argparse.ArgumentParser(description='rank and num of cpus')
+parser.add_argument('-rank')
+parser.add_argument('-num_cpus')
 
 args = parser.parse_args()
 
+# if rank == 0:
+#     data = {'a': args.my_argument}
+#     comm.send(data, dest=1, tag=11)
+#     print(f"this is from rank 0. I try to send the following: {data}")
+# elif rank == 1:
+#     data = comm.recv(source=0, tag=11)
+#     print(f"this is from rank 1. I have recieved the following: {data}")
 
-if rank == 0:
-    data = {'a': args.my_argument}
-    comm.send(data, dest=1, tag=11)
-    print(f"this is from rank 0. I try to send the following: {data}")
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
-    print(f"this is from rank 1. I have recieved the following: {data}")
+print(f'the following argument is passed: {args.rank}')
 
-print(f'the following argument is passed: {args.my_argument}')
+print('Now I will be doing cp2k...')
+
+run_folder = f'{args.rank}'  # will be created to copy there input file and run cp2k
+
+if not os.path.exists(run_folder):
+    os.mkdir(run_folder)
+
+cp2k_run(cp2k_executable='cp2k.popt',
+         run_type='mpi',
+         np=args.num_cpus,
+         xyz_file='input.inp',
+         output_file=f'out_{args.rank}.out',
+         error_file=f'err_{args.rank}.err',
+         execution_directory=f'{args.rank}',
+         )
+
