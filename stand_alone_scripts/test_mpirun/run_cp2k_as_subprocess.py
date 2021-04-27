6e8d157aac3b563df9f33fb03ca90549c0d38a6c
ArtemFediai <artem.fedyay@gmail.com>
2021-04-27 11:22:29 +0200

    Revert "nothing changed"
    This reverts commit 72342af8

--- stand_alone_scripts/test_mpirun/run_cp2k_as_subprocess.py
+++ stand_alone_scripts/test_mpirun/run_cp2k_as_subprocess.py
@@ -2,64 +2,33 @@
 subprocess mpi run of cp2k
 1. setulimit so far made no effect (local PC)
 2. commented lines and the function may be important for the cluster
-
-run with an argument:
-... -num_cpus 4
-
 """
 import os
 import subprocess
 import shlex
 import resource
-import argparse
-import shutil
+
 
 def main():
-
-
-    parser = argparse.ArgumentParser(description='number of cpus')
-    parser.add_argument('-num_cpus')  # number of cpus you request for every array job
-    parser.add_argument('-num_array_job')  # number of cpus you request for every array job
-    args = parser.parse_args()
-    num_cpus = args.num_cpus
-    num_array_job = args.num_array_job
-
-    # command = f'mpirun -np {num_cpus} /home/artem/soft/cp2k_test/cp2k-7.1/exe/local/cp2k.popt -i input.inp'
-
-    created_dir = str(num_array_job)
-    os.mkdir(created_dir)
-    shutil.copyfile('input.inp', created_dir + '/' + 'input.inp')
-    os.chdir(created_dir)
-
-
-    command = f'mpirun -np {num_cpus} /home/ws/bh5670/cp2k/cp2k-7.1/exe/local/cp2k.popt -i input.inp'
-    outstream = open('cp2k.out', 'w')
+    threads = 4
+    command = f'mpirun -np {threads} /home/artem/soft/cp2k/cp2k-7.1/exe/local/cp2k.popt -i input.inp'
+    outstream = open('my_out_file.out', 'w')
     execution_directory = os.path.abspath('.')
-    print('execution_directory', execution_directory)
 
     environ = os.environ.copy()
-    # environ["PARNODES"] = str(num_cpus)
+    # environ["PARNODES"] = str(threads)
     # environ["PARA_ARCH"] = "SMP"
     # environ["TM_PAR_FORK"] = "on"
-
-    # process = subprocess.Popen(shlex.split(command),
-    #                            stdin=subprocess.PIPE,
-    #                            stdout=outstream,
-    #                            stderr=outstream,
-    #                            preexec_fn=setulimit,
-    #                            cwd=execution_directory,
-    #                            encoding="utf-8",
-    #                            env=environ)
 
     process = subprocess.Popen(shlex.split(command),
                                stdin=subprocess.PIPE,
                                stdout=outstream,
                                stderr=outstream,
+                               preexec_fn=setulimit,
                                cwd=execution_directory,
                                encoding="utf-8",
                                env=environ)
-    process.wait()
-    # return process  # <-- this will crash!
+
     print('done')
 
 
