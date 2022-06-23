70a32697f54fb53f225729e137b74d866c56fca3
ArtemFediai <artem.fedyay@gmail.com>
2022-06-23 10:33:22 +0200

    outer scf added

--- mypycp2k/outer_scf.py
+++ mypycp2k/outer_scf.py
@@ -1,3 +1,3 @@
-def add_outer_scf(outer_scf, eps_scf=1.0E-10, max_scf=20):
-    outer_scf.Eps_scf = eps_scf
+def add_outer_scf(outer_scf, eps_scf=1.0E-10, max_scf=10):
+    # outer_scf.Eps_scf = eps_scf
     outer_scf.Max_scf = max_scf
